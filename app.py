2f6ace9267f9657c4552d8283de060382b661ce9
loomatrading <loomatrading@gmail.com>
2026-08-19 13:39:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -825,7 +825,7 @@ class DocumentScanner:
 # START PROGRAM
 # ==============================================================
 
-if _name_ == "_main_":
+if __name__ == '__main__':
 
     root = tk.Tk()
 
